26eae9687e376e56cc42fbfb0cec63b55faa3d46
Maggie Kikkert <margaretkikkert@trentu.ca>
2020-01-07 18:12:59 -0500

    Adding Steam Oauth (4)
    Added Steam authentication link - redirection to Steam's sign in page working, redirect back still not working

--- source/app/urls.py
+++ source/app/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('language/', ChangeLanguageView.as_view(), name='change_language'),
 
     path('accounts/', include('accounts.urls')),
+    path('auth/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
