c272f536a68ab5b415038a5a2370cb9e9eb27b9f
Sean Behan <codebam@riseup.net>
2020-02-02 13:55:04 -0500

    updated key generator function

--- source/keygen/keygen.py
+++ source/keygen/keygen.py
@@ -1,15 +1,10 @@
-import rsa
-from base64 import b64encode
+from os import urandom
+from hashlib import sha256
 
-def generate(unique, pubkey, privkey, secret="changeme"):
-    if None in [pubkey, privkey]:
-        pubkey, privkey = rsa.newkeys(4096)
-
-    data = str([unique, secret])
-    sig = rsa.sign(data.encode(), privkey, 'SHA-1')
-    license = b64encode(sig).decode('ascii').upper()[:32]
+def generate(unique, secret="changeme"):
+    r = urandom(4096)
+    data = str([unique, secret]) + str(r)
+    shasum = sha256(data.encode())
+    license = shasum.hexdigest().upper()[:32]
 
     return license
-
-from functools import partial
-gen_license = partial(generate, pubkey=your_pub_key, privkey=your_priv_key, secret="your_secret")
