5130b0e0c236fc7eeefbbce03fbd4814e30317ad
Sean Behan <codebam@riseup.net>
2020-02-03 16:35:30 -0500

    added new models to manage our subscriptions

--- source/license/models.py
+++ source/license/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
-class LicenseKey(models.Model):
-    code = models.CharField(max_length=100, primary_key=True)
+class Subscription(models.Model):
+    license_key = models.ForeignKey(License, on_delete=models.SET_NULL)
+    session_key = models.ForeignKey(Session, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Session(models.Model):
+    session_key = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class License(models.Model):
+    license_key = models.CharField(max_length=32, primary_key=True, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
