24643da7c16f5f5c3555a7c3a28552ae3d4b8b9c
Maggie Kikkert <margaretkikkert@trentu.ca>
2020-02-03 14:22:53 -0500

    Styling (17)
    Fixed error with restoring passwords

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -194,7 +194,7 @@ class RestorePasswordView(GuestOnlyView, FormView):
     def form_valid(self, form):
         user = form.user_cache
         token = default_token_generator.make_token(user)
-        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
 
         send_reset_password_email(self.request, user.email, token, uid)
 
