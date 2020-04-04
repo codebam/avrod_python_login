eacb7f67c595d2ea2a17f0f7cf0efb90885fd516
Maggie Kikkert <margaretkikkert@trentu.ca>
2020-04-03 20:41:58 -0400

    Users can now cancel and reactivate subscriptions

--- source/payment/urls.py
+++ source/payment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LicenseView, PaymentSuccessView, ManageLicenseView, UpdateSubscriptionView
+from .views import LicenseView, PaymentSuccessView, ManageLicenseView, UpdateSubscriptionView, CancelSubscriptionView, ReactivateSubscriptionView
 
 from . import views
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path('', LicenseView.as_view(), name='licensing'),
     path('manage/<str:sub_id>/', ManageLicenseView.as_view(), name='manage'),
     path('manage/<str:sub_id>/success/', UpdateSubscriptionView.as_view(), name='updated'),
+    path('manage/<str:sub_id>/cancel/', CancelSubscriptionView.as_view(), name='cancel-sub'),
+    path('manage/<str:sub_id>/reactivated/', ReactivateSubscriptionView.as_view(), name='reactivate'),
     path('success/', PaymentSuccessView.as_view(), name='checkout'),
     path('webhook/', views.webhook, name='webhook'),
 ]
--- source/payment/views.py
+++ source/payment/views.py
@@ -1,18 +1,19 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView
+from .models import Customer, Subscription, License
+from .keygen import generate
 import stripe
 import json
 import os
-from django.conf import settings
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Customer, Subscription, License
-from django.contrib.auth.models import User
-from .keygen import generate
-from django.utils.crypto import get_random_string
-from django.utils import timezone
 import datetime
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -30,6 +31,8 @@ class LicenseView(LoginRequiredMixin, TemplateView):
             subscription = Subscription.objects.filter(customer_id=customer_id)
             if subscription.exists():
                 subscription = Subscription.objects.get(customer_id=customer_id)
+                last_period = stripe.Subscription.retrieve(subscription.sub_id).cancel_at_period_end
+                request.session['sub_last_period'] = last_period
                 return redirect('payment:manage', sub_id=subscription.sub_id)
             else:
                 session = stripe.checkout.Session.create(
@@ -69,6 +72,79 @@ class LicenseView(LoginRequiredMixin, TemplateView):
         return render(request, self.template_name, context)
         
 
+class PaymentSuccessView(LoginRequiredMixin, TemplateView):
+    template_name = 'success.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class ManageLicenseView(LoginRequiredMixin, TemplateView):
+    template_name = 'manage.html'
+
+    def get(self, request, *args, **kwargs):
+        sub_id = kwargs.get('sub_id')
+        last_period = request.session.get('sub_last_period')
+        subscription = Subscription.objects.get(sub_id=sub_id)
+
+        session = stripe.checkout.Session.create(
+            customer=subscription.customer_id.customer_id,
+            payment_method_types=['card'],
+            mode='setup',
+            setup_intent_data={
+                'metadata': {
+                    'subscription_id': sub_id
+                },
+            },
+            success_url='http://localhost:8000/licensing/manage/'+sub_id+'/success/',
+            cancel_url='http://localhost:8000/licensing/manage/'+sub_id+'/',
+            metadata={
+                'avrod_id': self.request.user.id,
+            }
+        )
+
+        context = {
+            'session_id': session.id,
+            'subscription': subscription,
+            'last_period': last_period,
+        }
+        return render(request, self.template_name, context)
+
+
+class CancelSubscriptionView(LoginRequiredMixin, TemplateView):
+    template_name = 'cancel.html'
+    
+    def get(self, request, *args, **kwargs):
+        sub_id = kwargs.get('sub_id')
+        subscription = Subscription.objects.get(sub_id=sub_id)
+
+        stripe.Subscription.modify(
+            sub_id,
+            cancel_at_period_end=True
+        )
+
+        return render(request, self.template_name)
+
+
+class UpdateSubscriptionView(LoginRequiredMixin, TemplateView):
+    template_name = 'update-success.html'
+
+
+class ReactivateSubscriptionView(LoginRequiredMixin, TemplateView):
+    template_name = 'reactivate.html'
+
+    def get(self, request, *args, **kwargs):
+        sub_id = kwargs.get('sub_id')
+        subscription = Subscription.objects.get(sub_id=sub_id)
+
+        stripe.Subscription.modify(
+            sub_id,
+            cancel_at_period_end=False,
+        )
+
+        return render(request, self.template_name)
+
+
 # Webhook Receiver
 endpoint_secret = 'whsec_P2OxXQZFSLJ1F5kgPULOEKa0DcoqjVJj'
 @csrf_exempt
@@ -92,6 +168,7 @@ def webhook(request):
     if event['type'] == 'checkout.session.completed':
         session = event['data']['object']
         print("checkout.session.completed")
+        print(session)
 
         user = User.objects.get(id = session.metadata.avrod_id)
         print(user)
@@ -100,6 +177,7 @@ def webhook(request):
         print(customer)
         customer_id = session.customer
         if not customer.exists():
+            print('creating customer')
             # Create customer in Django
             customer = Customer.create(
                 customer_id,
@@ -107,39 +185,47 @@ def webhook(request):
             )
             customer.save()
         else:
+            print('retrieving customer')
             customer = Customer.objects.get(user_id=user)
-        
-        key = generate(get_random_string(32))
-        license_key = License.create(key)
-        license_key.save()
-        stripe_sub = stripe.Subscription.retrieve(session.subscription)
-        sub_end_date = datetime.datetime.fromtimestamp(float(stripe_sub.current_period_end)).date()
 
-        subscription = Subscription.create(
-            stripe_sub.id,
-            customer,
-            license_key,
-            sub_end_date
-        )
-        subscription.save()
+        subscription = Subscription.objects.filter(customer_id=customer.customer_id)
+        print(subscription)
+        if subscription.exists():
+            print('updating payment method')
+            # User is updating their card information
+            intent_id = session.setup_intent
+            intent = stripe.SetupIntent.retrieve(intent_id)
+            print(intent)
+            print(stripe.Customer.retrieve(intent.customer))
+            stripe.Customer.modify(
+                customer.customer_id,
+                invoice_settings={'default_payment_method': intent.payment_method}
+            )
+            print('updated subscription')
+            return HttpResponse(status=200)
+        else:
+            print('creating subscription')
+            # User needs a subscription
+            key = generate(get_random_string(32))
+            license_key = License.create(key)
+            license_key.save()
+            stripe_sub = stripe.Subscription.retrieve(session.subscription)
+            sub_end_date = datetime.datetime.fromtimestamp(float(stripe_sub.current_period_end)).date()
+
+            subscription = Subscription.create(
+                stripe_sub.id,
+                customer,
+                license_key,
+                sub_end_date
+            )
+            subscription.save()
+
+    elif event['type'] == 'customer.subscription.deleted':
+        session = event['data']['object']
+        customer = session.customer
+
+        subscription = Subscription.objects.get(customer_id=customer)
+        License.objects.filter(license_key=subscription.license_key).delete()
+        Subscription.objects.filter(sub_id=subscription.sub_id).delete()
 
     return HttpResponse(status=200)
-
-
-class PaymentSuccessView(LoginRequiredMixin, TemplateView):
-    template_name = 'success.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
-
-
-class ManageLicenseView(LoginRequiredMixin, TemplateView):
-    template_name = 'manage.html'
-
-    def get(self, request, *args, **kwargs):
-        sub_id = kwargs.get('sub_id')
-        subscription = Subscription.objects.get(sub_id=sub_id)
-        context = {
-            'subscription': subscription
-        }
-        return render(request, self.template_name, context)
